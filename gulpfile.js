257ba02a56a4897f6cb79dba6312fd517d3cf1ba
TJ Nicolaides <tjnicolaides@gmail.com>
2014-11-13 23:06:12 -0500

    Adding a sass task with gulp-ruby-sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,9 @@
 var gulp = require('gulp');
+var sass = require('gulp-ruby-sass');
 
-gulp.task('default', function() {
-	console.log('hello world');
+gulp.task('sass', function () {
+    return gulp.src('scss/main.scss')
+        .pipe(sass({sourcemap: true, sourcemapPath: '../scss'}))
+        .on('error', function (err) { console.log(err.message); })
+        .pipe(gulp.dest('css'));
 });
