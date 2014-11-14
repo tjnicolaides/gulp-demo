3571b95e57c2d6ec0aa0af0be9ccac5bda436159
TJ Nicolaides <tjnicolaides@gmail.com>
2014-11-13 23:13:44 -0500

    Cleaning directories with del

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,10 @@
 var gulp = require('gulp');
 var sass = require('gulp-ruby-sass');
+var del = require('del');
+
+gulp.task('clean', function (cb) {
+  del(['css/**'], cb);
+});
 
 gulp.task('sass', function () {
     return gulp.src('scss/main.scss')
@@ -7,3 +12,4 @@ gulp.task('sass', function () {
         .on('error', function (err) { console.log(err.message); })
         .pipe(gulp.dest('css'));
 });
+
