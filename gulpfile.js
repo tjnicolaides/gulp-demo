bb083585dc42a16b1a019992e3e040c6de8d144d
TJ Nicolaides <tjnicolaides@gmail.com>
2014-11-13 23:35:50 -0500

    Concatenating, uglifying, renaming JS (in order!)

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,25 @@
 var gulp = require('gulp');
 var sass = require('gulp-ruby-sass');
 var del = require('del');
+var concat = require('gulp-concat');
+var rename = require('gulp-rename');
+var uglify = require('gulp-uglify');
+var order = require("gulp-order");
+
+
+// Concat & Minify JS
+gulp.task('minify', function(){
+  return gulp.src('js/src/**/*.js')
+  	.pipe(order([
+        'js/src/vendor/jquery-1.11.1.min.js',
+        'js/src/**/*.js'
+    ], { base: './' }))
+    .pipe(concat('all.js'))
+    .pipe(gulp.dest('js/dist'))
+    .pipe(rename('all.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('js/dist'));
+});
 
 gulp.task('clean', function (cb) {
   del(['css/**'], cb);
