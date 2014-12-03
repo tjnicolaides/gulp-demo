3104ff5d0afdfa0069d63f44d89bb7aa45bf3884
TJ Nicolaides <tjnicolaides@gmail.com>
2014-12-02 23:44:20 -0500

    Our sass task

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,12 @@
 var gulp = require('gulp');
+var sass = require('gulp-ruby-sass');
+
+gulp.task('sass', function () {
+    return gulp.src('scss/main.scss')
+    	.pipe(sass())
+        .on('error', function (err) { console.log(err.message); })
+        .pipe(gulp.dest('css'));
+});
 
 gulp.task('default', function(){
 	console.log('hello world!');
