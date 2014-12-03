b6c5f722cd0b76eff01dfdfe0f3b31ee1860255d
TJ Nicolaides <tjnicolaides@gmail.com>
2014-12-02 23:46:00 -0500

    Clean task can empty a directory for us

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,16 @@
 var gulp = require('gulp');
 var sass = require('gulp-ruby-sass');
+var del = require('del');
 
 gulp.task('sass', function () {
     return gulp.src('scss/main.scss')
     	.pipe(sass())
         .on('error', function (err) { console.log(err.message); })
         .pipe(gulp.dest('css'));
+});
+
+gulp.task('clean', function (cb) {
+	del(['css/**'], cb);
 });
 
 gulp.task('default', function(){
